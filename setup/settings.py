72f45db9a977d17e7966e5a8a229e2296de17cc8
Respect-J <respect0170@gmail.com>
2024-02-01 14:14:31 +0500

    added media root

--- setup/settings.py
+++ setup/settings.py
@@ -158,4 +158,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = "/home/theeastc/www/back/media"
