476b44dd45a820f7e4645b60383f07e1482de7fd
Respect-J <respect0170@gmail.com>
2024-02-03 20:28:28 +0500

    add swager

--- apps/index/urls.py
+++ apps/index/urls.py
@@ -3,5 +3,5 @@ from .views import CountryListView, StepbyStepListView, ExcludedListView
 urlpatterns = [
     path('countre/', CountryListView.as_view(), name='CountryListView'),
     path('step/', StepbyStepListView.as_view(), name='StepbyStepListView'),
-    path('step/', ExcludedListView.as_view(), name='ExcludedListView'),
+    path('excluded/', ExcludedListView.as_view(), name='ExcludedListView'),
 ]
--- setup/urls.py
+++ setup/urls.py
@@ -16,8 +16,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf import settings
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="AladinBACK",
+        default_version='v1',
+        description="Your API description",
+        terms_of_service="https://back.theeastcaravan.com/",
+        contact=openapi.Contact(email="contact@yourapp.com"),
+        license=openapi.License(name="NOT"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 
 admin.site.site_header = 'THE EAST TRAVEL'
@@ -27,6 +43,10 @@ admin.site.index_title = 'Добро пожаловать в администр�
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apps.index.urls')),
-    path('tours/', include('apps.tours.urls'))
+    path('tours/', include('apps.tours.urls')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0),
+                          name='schema-json'),
+    re_path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
